1f1960772025e2ff5ec08dd7bfdbebe46f18dc12
Yauheny-Bychkou <55757666+Yauheny-Bychkou@users.noreply.github.com>
2022-04-08 22:33:02 +0300

    fix: name

--- src/modules/components/greeting/greeting.js
+++ src/modules/components/greeting/greeting.js
@@ -11,7 +11,7 @@ class Greeting {
     <img class="greeting-img" src="mainn.jpg" alt="main">
      <div class="greeting-wrapper">
         <h1 class="greeting-wrapper__title">Woodbag.s</h1>
-        <h2 class="greeting-wrapper__name">Деревянные шедевры ручной&nbsp;работы!</h2>
+        <h2 class="greeting-wrapper__name">Деревянные шедевры ручной&nbsp;работы</h2>
         <button class="greeting-wrapper__button">Галерея</button>
       </div>
     `;
