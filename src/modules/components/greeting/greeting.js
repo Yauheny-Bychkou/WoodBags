a868914807e466e4464a0392a9b8f37e44d08c82
Yauheny-Bychkou <55757666+Yauheny-Bychkou@users.noreply.github.com>
2022-04-08 23:59:45 +0300

    fix: mistakes

--- src/modules/components/greeting/greeting.js
+++ src/modules/components/greeting/greeting.js
@@ -8,11 +8,11 @@ class Greeting {
   }
   getCode() {
     return `
-    <img class="greeting-img" src="mainn.jpg" alt="main">
+    <img class="greeting-img" src="main.jpg" alt="main">
      <div class="greeting-wrapper">
         <h1 class="greeting-wrapper__title">Woodbag.s</h1>
         <h2 class="greeting-wrapper__name">Деревянные шедевры ручной&nbsp;работы</h2>
-        <button class="greeting-wrapper__button">Галерея</button>
+        <a href="#" class="greeting-wrapper__button">Каталог</a>
       </div>
     `;
   }
